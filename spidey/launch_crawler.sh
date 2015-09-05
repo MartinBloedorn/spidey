10b31c132c1d91afb6132b817c89d2115923078f
Martin Bloedorn <martin.bloedorn@gmail.com>
2015-09-05 19:34:02 -0300

    Fixing (?) launch_crawler.sh

--- spidey/launch_crawler.sh
+++ spidey/launch_crawler.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-cd spidey_crawler
-/usr/local/bin/scrapy crawl gizmodo > gizmodo_crawler_log.txt 2>&1
-cd .. 
+
+# Currently no smart workaround this
+SPIDEY_DIR=/home/martin/apps/spidey.git
+SCRAPY_BIN=/usr/local/bin/scrapy
+
+# Set automatically
+SCWD="$(pwd)"
+
+cd $SPIDEY_DIR/spidey/spidey_crawler
+${SCRAPY_BIN} crawl gizmodo > gizmodo_crawler_log.txt 2>&1
+cd $SCWD
