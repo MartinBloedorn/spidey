cb25f1ea7574af868fc4bf89f9db81bf4ed0780c
Martin Bloedorn <martin.bloedorn@gmail.com>
2015-09-06 23:24:46 -0300

    Changing some bits in launch_crawler.sh

--- spidey/launch_crawler.sh
+++ spidey/launch_crawler.sh
@@ -3,10 +3,11 @@
 # Currently no smart workaround this
 SPIDEY_DIR=/home/martin/apps/spidey.git
 SCRAPY_BIN=/usr/local/bin/scrapy
+SCRAPY_LOG=/home/martin/apps/gizmodo_crawler_log.txt
 
 # Set automatically
 SCWD="$(pwd)"
 
 cd $SPIDEY_DIR/spidey/spidey_crawler
-${SCRAPY_BIN} crawl gizmodo > gizmodo_crawler_log.txt 2>&1
+${SCRAPY_BIN} crawl gizmodo -s LOG_FILE=$SCRAPY_LOG
 cd $SCWD
