a67c3175548c63702d7b66396f392d2f91f858e6
Martin Bloedorn <martin.bloedorn@gmail.com>
2015-09-07 23:39:05 -0300

    Django Debug=False

--- spidey/spidey_django/settings.py
+++ spidey/spidey_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'squ_n!n)c+d2tb(x4rqssnp1bhdrn350d7k*(z%miw$w)ynuv6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '104.236.95.141', 'martinvb.com']
 
